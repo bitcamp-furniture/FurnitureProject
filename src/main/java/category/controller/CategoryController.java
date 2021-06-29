4b615bc6a88234091292d104b3f8b1768a4085d5
wingna1 <wingnawing@gmail.com>
2021-06-29 16:13:01 +0900

    이벤트완성, faq 작업시작, 상품등록 pull merge받음

--- src/main/java/category/controller/CategoryController.java
+++ src/main/java/category/controller/CategoryController.java
@@ -43,60 +43,6 @@ public class CategoryController {
 		return "/index";
 	}
 
-	// 썸네일 + 디테일 이미지 등록
-	@RequestMapping(value = "/productboardWrite", method = RequestMethod.POST)
-	@ResponseBody
-	public void productboardWrite(@ModelAttribute ProductDTO productDTO, @RequestParam MultipartFile thumbImg,
-			@RequestParam("detailImg[]") List<MultipartFile> list, Model model) {
-
-		String filePath = "C:\\Users\\LeeMinJeong\\Desktop\\FurnitureProject\\src\\main\\webapp\\category\\storage";
-		String fileName = thumbImg.getOriginalFilename();
-		File file = new File(filePath, fileName); // 파일 생성
-
-		// 파일 복사
-		// 썸네일 이미지(이미지1개)
-		try {
-			FileCopyUtils.copy(thumbImg.getInputStream(), new FileOutputStream(file));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		productDTO.setProduct_img_thumb(fileName);
-
-		// 디테일 이미지(이미지 여러개)
-		for (MultipartFile detailImg : list) {
-			fileName = detailImg.getOriginalFilename();
-			file = new File(filePath, fileName);
-
-			// 파일복사
-			try {
-				FileCopyUtils.copy(detailImg.getInputStream(), new FileOutputStream(file));
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
-			productDTO.setProduct_img_detail(fileName);
-
-			// DB DB에 한줄 쫘르르륵 들어간다
-			productService.categoryboardWrite(productDTO);
-		} // for
-
-	}
-
-	// 전체 상품을 리스트로 가져올 때에 display2에 띄우는 것 >> /list로 연결됨
-	// display display2 화면에 띄워지는 곳
-	@RequestMapping(value = "/categoryAllList", method = RequestMethod.GET) // Resolver를 타고 가지말아라
-	public String categoryAllList(@RequestParam(required = false, defaultValue = "1") String pg,
-								  Model model) 
-	{
-		model.addAttribute("pg", pg);
-		model.addAttribute("display", "/category/view/category.jsp");
-		model.addAttribute("categorydisplay", "/category/view/allList.jsp");
-		return "/index";
-
-	}
-
-	
 	// 전체 상품을 리스트로 가져오는 메소드 +페이징 필요 없음 
 	@ResponseBody
 	@RequestMapping(value = "/getAllList", method = RequestMethod.POST)
